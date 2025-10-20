04b33a94aa5ac7ef645deff0767a11a4af0080d4
Steven <stevenchicue@hotmail.com>
2025-10-20 17:41:42 -0500

    Agregar archivo de configuración con ajustes de CORS, base de datos y zona horaria

---
+++ app/core/config.py
@@ -0,0 +1,34 @@
+import os
+from zoneinfo import ZoneInfo
+
+
+
+class Settings:
+    # CORS
+    ALLOW_ORIGINS = [
+        "http://localhost:5173",
+        "http://127.0.0.1:8000",
+        "https://bv2x6hss-8000.use2.devtunnels.ms/",
+    ]
+    TZ_INFO = os.getenv("TZ_INFO", "UTC")
+    
+    # DATABASE PostgreSQL
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+    POSTGRES_URL = (f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}")
+
+    # JWT
+    JWT_SECRET = os.getenv("JWT_SECRET")
+    JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
+    REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
+    
+    # Time Zone
+    TIMEZONE = ZoneInfo(TZ_INFO)
+
+
+
+settings = Settings()
