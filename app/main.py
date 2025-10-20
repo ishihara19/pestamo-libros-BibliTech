d7c37962663401b7abf4641150bf2fb111b9b24a
Steven <stevenchicue@hotmail.com>
2025-10-20 17:06:58 -0500

    Agregar manejo de zona horaria y mejorar respuesta en la ruta raíz de la API para pruebas de varibles de entorno

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-
+import os
+from datetime import datetime
 @asynccontextmanager
 async def lifespan(app: FastAPI):
 
     print("Servidor iniciando...")
-   
+    
+    tz = os.getenv("TZ_INFO", "UTC")
+    print(f"Configurando zona horaria a: {tz}")
 
    
     yield
@@ -16,4 +19,9 @@ app = FastAPI(lifespan=lifespan,title="API BibliTech", description="API RESTful 
 
 @app.get("/")
 async def root():
-    return {"message": "Bienvenido a la API BibliTech"}
+    tz = os.getenv("TZ_INFO", "UTC")
+    now_utc = datetime.now()
+    now_tz = datetime.now(tz)
+    return {"message": "Bienvenido a la API BibliTech",
+            "now_utc":now_utc,
+            "now_tz":now_tz}
