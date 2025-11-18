3cb3c668418f8cbe266166512a21570bff1d0910
Steven <stevenchicue@hotmail.com>
2025-11-18 17:51:13 -0500

    Agregar modelo, rutas y servicio para gestionar préstamos de libros, incluyendo creación y listado de préstamos.

--- app/schemas/prestamo_sch.py
+++ app/schemas/prestamo_sch.py
@@ -4,21 +4,88 @@ from typing import Optional
 from ..utils.tiempo_tz import to_localtime
 
 class HacerPrestamo(BaseModel):
-    
-    usuario_id: int = Field(..., description="ID del usuario que realiza el préstamo")
-    ejemplar_id: int = Field(..., description="ID del ejemplar que se presta")
-    fecha_solicitud: datetime = Field(..., description="Fecha en que se realiza el préstamo")
-    
-class ValidarPrestamo(BaseModel):
-    
-    documento: str = Field(..., description="Documento del usuario que realiza el préstamo")
-     
 
-class ValidarPrestamoResponse(BaseModel):
-    nombre_usuario: str
-    apellido_usuario: str
-    correo_usuario: str
-    titulo_libro: str
-    codigo_interno_ejemplar: str
+    libro_id: int = Field(..., description="ID del libro que se presta")
+    fecha_solicitud: datetime = Field(..., description="Fecha en que se realiza el préstamo")
+    dias_prestamo: int = Field(..., description="Días de préstamo solicitados")
+    
+
+class PrestamoViewBibliotecario(BaseModel):
+    
+    id: int
+    usuario_id: int
+    ejemplar_id: int
     fecha_solicitud: datetime
-    fecha_prevista_devolucion: date   
+    fecha_prevista_devolucion: date
+    fecha_entrega: Optional[date]
+    fecha_devuelto: Optional[date]
+    creado_en: datetime
+    actualizado_en: Optional[datetime]
+    
+    model_config = ConfigDict(from_attributes=True)
+    @classmethod
+    def model_validate(cls, obj) -> "PrestamoViewBibliotecario":
+        instance = super().model_validate(obj)
+        # Solo convertir para mostrar
+        instance.fecha_solicitud = to_localtime(instance.fecha_solicitud)
+        instance.creado_en = to_localtime(instance.creado_en)
+        instance.actualizado_en = to_localtime(instance.actualizado_en)
+        return instance
+
+
+class PrestamoViewNormalizedBibliotecario(BaseModel):
+    usuario_nombre: str
+    documento_usuario: str
+    libro_titulo: str
+    ejemplar_codigo_interno:str
+    estado_ejemplar: str
+    fecha_solicitud: datetime
+    fecha_entrega: Optional[date]
+    fecha_prevista_devolucion: date 
+    fecha_devuelto: Optional[date]
+    creado_en: datetime
+    actualizado_en: Optional[datetime]
+    
+    model_config = ConfigDict(from_attributes=True)
+    
+    @classmethod
+    def from_model(cls, prestamo):
+        return cls(
+            documento_usuario=prestamo.usuarios.numero_documento,
+            fecha_solicitud=prestamo.fecha_solicitud,
+            fecha_prevista_devolucion=prestamo.fecha_prevista_devolucion,
+            fecha_entrega=prestamo.fecha_entrega,
+            fecha_devuelto=prestamo.fecha_devuelto,
+            creado_en=prestamo.creado_en,
+            actualizado_en=prestamo.actualizado_en,
+            usuario_nombre=f"{prestamo.usuarios.nombre} {prestamo.usuarios.apellido}",
+            libro_titulo=prestamo.ejemplar.libro.titulo,
+            ejemplar_codigo_interno=prestamo.ejemplar.codigo_interno,
+            estado_ejemplar=prestamo.ejemplar.estado.nombre,
+        )
+        
+
+class PrestamoViewNormalizedLector(BaseModel):
+    
+    libro_titulo: str
+    ejemplar_codigo_interno:str
+    estado_ejemplar: str
+    fecha_solicitud: datetime
+    fecha_entrega: Optional[date]
+    fecha_prevista_devolucion: date 
+    fecha_devuelto: Optional[date]
+        
+    model_config = ConfigDict(from_attributes=True)
+    
+    @classmethod
+    def from_model(cls, prestamo):
+        return cls(
+           
+            fecha_solicitud=prestamo.fecha_solicitud,
+            fecha_prevista_devolucion=prestamo.fecha_prevista_devolucion,
+            fecha_entrega=prestamo.fecha_entrega,
+            fecha_devuelto=prestamo.fecha_devuelto,           
+            libro_titulo=prestamo.ejemplar.libro.titulo,
+            ejemplar_codigo_interno=prestamo.ejemplar.codigo_interno,
+            estado_ejemplar=prestamo.ejemplar.estado.nombre,
+        )        
---
+++ app/services/prestamo_service.py
@@ -0,0 +1,198 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func, update
+from fastapi import HTTPException
+from datetime import datetime, timedelta, timezone
+from sqlalchemy.orm import selectinload
+
+
+from ..models.prestamo import Prestamo
+from ..models.usuario import Usuario
+from ..models.ejemplar import Ejemplar
+from ..core.db.postgre import set_app_context, clear_app_context
+from ..utils.utils import generar_fecha_devolucion_prevista, ejemplar_disponible_info
+from ..schemas.paginacion_sch import PaginationParams, PaginatedResponse
+from ..schemas.prestamo_sch import HacerPrestamo, PrestamoViewBibliotecario, PrestamoViewNormalizedBibliotecario, PrestamoViewNormalizedLector
+from ..schemas.generic_sch import GenericMessage
+from ..core.config import settings
+
+
+class PrestamoService:
+    @staticmethod
+    async def crear_prestamo(
+        db: AsyncSession,
+        prestamo_data: HacerPrestamo,
+        usuario_id: int,
+        ip: str,
+        host: str,
+        username: str,
+    ) -> GenericMessage:
+        """Crear un nuevo préstamo"""
+        try:
+            # Establecer contexto de la aplicación
+            await set_app_context(db, username, ip, host, "crear_prestamo")
+            # Generar fecha prevista de devolución
+            fecha_prevista_devolucion = await generar_fecha_devolucion_prevista(
+                prestamo_data.fecha_solicitud.date(), prestamo_data.dias_prestamo
+            )
+            # Verificar disponibilidad del ejemplar
+            ejemplar = await ejemplar_disponible_info(prestamo_data.libro_id, db)
+
+            if ejemplar is None:
+                raise HTTPException(
+                    status_code=400,
+                    detail="No hay ejemplares disponibles para el libro solicitado.",
+                )
+            # Crear el préstamo
+            nuevo_prestamo = Prestamo(
+                usuario_id=usuario_id,
+                ejemplar_id=ejemplar["id"],
+                fecha_solicitud=prestamo_data.fecha_solicitud,
+                fecha_prevista_devolucion=fecha_prevista_devolucion,
+            )
+            # Guardar en la base de datos
+            db.add(nuevo_prestamo)
+            # Cambiar el estado del ejemplar a 'Reservado'
+            stmt = (
+                update(Ejemplar)
+                .where(Ejemplar.id == ejemplar["id"])
+                .values(estado_id=settings.RESERVADO_EJEMPLAR_ID)
+            )
+            # Ejecutar las operaciones
+            await db.execute(stmt)
+            # Confirmar los cambios
+            await db.commit()
+
+            return GenericMessage(
+                message=f"Préstamo creado exitosamente con el ejemplar {ejemplar['codigo_interno']}."
+            )
+
+        except Exception as e:
+            
+            await db.rollback()
+            raise
+
+        finally:
+
+            await clear_app_context(db)
+
+
+    async def listar_prestamos_bibliotecario(
+        db: AsyncSession,
+        pagination: PaginationParams | None = None,
+        normalizado: bool = False,
+    ) ->(
+        list[PrestamoViewBibliotecario]
+        | list[PrestamoViewNormalizedBibliotecario]
+        | PaginatedResponse[PrestamoViewBibliotecario]
+        | PaginatedResponse[PrestamoViewNormalizedBibliotecario]
+    ):
+        """Listar todos los préstamos realizados."""
+        if pagination:
+            # Contar total de registros
+            count_query = select(func.count(Prestamo.id))
+            total_result = await db.execute(count_query)
+            total = total_result.scalar()
+            if normalizado:
+                # Obtener registros paginados
+                query = (
+                    select(Prestamo)
+                    .options(
+                        selectinload(Prestamo.usuarios),
+                        selectinload(Prestamo.ejemplar).selectinload(Ejemplar.libro),
+                        selectinload(Prestamo.ejemplar).selectinload(Ejemplar.estado),
+                    )
+                    .offset(pagination.offset)
+                    .limit(pagination.limit)
+                )
+                result = await db.execute(query)
+                prestamos = result.scalars().all()
+
+                items = [
+                    PrestamoViewNormalizedBibliotecario.from_model(prestamo)
+                    for prestamo in prestamos
+                ]
+                return PaginatedResponse.create(items, total, pagination)
+            # Obtener registros paginados
+            query = (
+                select(Prestamo)
+                .offset(pagination.offset)
+                .limit(pagination.limit)
+            )
+            result = await db.execute(query)
+            prestamos = result.scalars().all()
+
+            items = [PrestamoViewBibliotecario.model_validate(prestamo) for prestamo in prestamos]
+            return PaginatedResponse.create(items, total, pagination)
+
+        if normalizado:
+            # Obtener todos los registros
+            query = select(Prestamo).options(
+                selectinload(Prestamo.usuarios),
+                selectinload(Prestamo.ejemplar).selectinload(Ejemplar.libro),
+                selectinload(Prestamo.ejemplar).selectinload(Ejemplar.estado),
+            )
+            result = await db.execute(query)
+            prestamos = result.scalars().all()
+
+            return [
+                PrestamoViewNormalizedBibliotecario.from_model(prestamo)
+                for prestamo in prestamos
+            ]
+        # Obtener todos los registros
+        query = select(Prestamo)
+        result = await db.execute(query)
+        prestamos = result.scalars().all()
+
+        return [PrestamoViewBibliotecario.model_validate(prestamo) for prestamo in prestamos]
+
+    @staticmethod
+    async def listar_prestamos_lector(
+        db: AsyncSession,
+        usuario_id: int,
+        pagination: PaginationParams | None = None,        
+    ) -> list[PrestamoViewNormalizedLector]:
+
+        if pagination:
+            # Contar total
+            count_query = select(func.count(Prestamo.id)).where(Prestamo.usuario_id == usuario_id)
+            total_result = await db.execute(count_query)
+            total = total_result.scalar()
+
+            # Consulta con relaciones cargadas
+            query = (
+                select(Prestamo)
+                .where(Prestamo.usuario_id == usuario_id)
+                .options(
+                    selectinload(Prestamo.ejemplar).selectinload(Ejemplar.libro),
+                    selectinload(Prestamo.ejemplar).selectinload(Ejemplar.estado),
+                )
+                .offset(pagination.offset)
+                .limit(pagination.limit)
+            )
+
+            result = await db.execute(query)
+            prestamos = result.scalars().all()
+
+            items = [
+                PrestamoViewNormalizedLector.from_model(prestamo)
+                for prestamo in prestamos
+            ]
+            return PaginatedResponse.create(items, total, pagination)
+     
+        query = (
+            select(Prestamo)
+            .where(Prestamo.usuario_id == usuario_id)
+            .options(
+                selectinload(Prestamo.ejemplar).selectinload(Ejemplar.libro),
+                selectinload(Prestamo.ejemplar).selectinload(Ejemplar.estado),
+            )
+        )
+
+        result = await db.execute(query)
+        prestamos = result.scalars().all()
+
+        return [
+            PrestamoViewNormalizedLector.from_model(prestamo)
+            for prestamo in prestamos
+        ]
+
